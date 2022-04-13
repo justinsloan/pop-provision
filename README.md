8beb7dfe2920ea5c36efdef5db919d4c6c773dc6
Justin <hello@justinsloan.com>
2022-04-12 17:33:09 -1000

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is a custom bash script I use to provision a new Debian-based Linux Destop. You can read more about it [here](https://www.justinsloan.com).
 
 *Use extreme caution before executing a bash script from a third party.*
+
+    sudo curl https://raw.githubusercontent.com/justinsloan/provision/main/provision.bash | bash
--- provision.sh
+++ provision.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+# Superuser permission required.
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-apt --purge remove firefox
-
+sudo apt --purge remove firefox firefox-esr chromium epiphany evolution
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
 curl https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor > vscodium.gpg
-install -o root -g root -m 644 vscodium.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64] https://download.vscodium.com/debs vscodium main" > /etc/apt/sources.list.d/vscodium.list'
+sudo install -o root -g root -m 644 vscodium.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://download.vscodium.com/debs vscodium main" > /etc/apt/sources.list.d/vscodium.list'
 
-apt update 
-apt upgrade
-apt autoremove
+sudo apt update 
+sudo apt upgrade
+sudo apt autoremove
 
-apt install htop tmux git nmap barrier codium python3-pip twine zotero virtualbox transmission keepassxc cmatrix curtail imagemagick nautilus-image-converter ttf-mscorefonts-installer microsoft-edge-stable
+sudo apt install htop tmux git nmap barrier codium python3-pip twine zotero virtualbox transmission keepassxc cmatrix curtail imagemagick nautilus-image-converter ttf-mscorefonts-installer microsoft-edge-stable
 
+# Install Microsoft Fonts
 mkdir ~/.fonts
 
 set -e
@@ -57,6 +58,6 @@ cabextract -L -F '*.TT[FC]' -d ~/.fonts "$TMPDIR/ppviewer.cab"
 
 fc-cache -fv ~/.fonts
 
-rm ~/Downloads/$ARCHIVE
+sudo rm ~/Downloads/$ARCHIVE
 
 echo "Provisioning of this system is complete."
