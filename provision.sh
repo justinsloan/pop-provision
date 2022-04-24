ff2a4a620cbcc71f12d4f175e03742bff8d25c26
Justin <hello@justinsloan.com>
2022-04-23 15:33:49 -1000

    daily update

--- provision.sh
+++ provision.sh
@@ -69,7 +69,6 @@ sudo apt -y install microsoft-edge-stable
 pip3 install quantumdiceware
 
 # Install Codium Extensions
-sudo -u $SUDO_USER codium --no-sandbox --user-data-dir /home/$SUDO_USER
 sudo -u $SUDO_USER codium --install-extension sleistner.vscode-fileutils
 sudo -u $SUDO_USER codium --install-extension streetsidesoftware.code-spell-checker
 sudo -u $SUDO_USER codium --install-extension ms-python.python
