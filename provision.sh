47ca34c316a3dd7f3849a00b30de79d673b9fe54
Justin <justinsloan@users.noreply.github.com>
2023-04-11 16:47:18 -1000

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -50,6 +50,11 @@ sudo apt -y purge epiphany-browser
 sudo apt -y purge evolution 
 
 # Install Additional Repositories
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
+rm microsoft.gpg
+
 curl https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor > vscodium.gpg
 sudo install -o root -g root -m 644 vscodium.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://download.vscodium.com/debs vscodium main" > /etc/apt/sources.list.d/vscodium.list'
@@ -83,7 +88,6 @@ sudo apt -y install twine
 sudo apt -y install remmina
 sudo apt -y install inetutils-traceroute
 sudo apt -y install traceroute
-sudo apt -y install keepassxc 
 sudo apt -y install torbrowser-launcher
 sudo apt -y install cmatrix 
 sudo apt -y install neofetch
@@ -91,7 +95,7 @@ sudo apt -y install curtail
 sudo apt -y install imagemagick 
 sudo apt -y install nautilus-image-converter
 sudo apt -y install gnome-tweaks 
-sudo apt -y install vivaldi-stable
+sudo apt -y install microsoft-edge-stable
 sudo apt -y install onedriver
 
 # Install Python Packages
