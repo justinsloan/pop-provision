77cdfbe3045d2521899c6a4c3a19a12510d1db18
Justin <justinsloan@users.noreply.github.com>
2024-09-02 09:20:00 -1000

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -155,6 +155,8 @@ sudo mv micro /usr/bin
 
 # Install Ollama
 curl -fsSL https://ollama.com/install.sh | sh
+echo 'Environment="HSA_OVERRIDE_GFX_VERSION=11.0.0"' | sudo tee /etc/systemd/system/ollama.service.d/override.conf
+sudo systemctl daemon-reload
 
 # Install Microsoft Fonts
 sudo -u $SUDO_USER mkdir /home/$SUDO_USER/.fonts 
